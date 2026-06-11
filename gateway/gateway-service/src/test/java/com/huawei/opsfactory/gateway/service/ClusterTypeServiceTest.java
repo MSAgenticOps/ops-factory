46835c7f23e9a4544cb4864dba49e61f8b240847
zhulin-ms <737165056@qq.com>
2026-06-11 08:36:26 +0800

    安全整改

--- gateway/gateway-service/src/test/java/com/huawei/opsfactory/gateway/service/ClusterTypeServiceTest.java
+++ gateway/gateway-service/src/test/java/com/huawei/opsfactory/gateway/service/ClusterTypeServiceTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import com.huawei.opsfactory.gateway.config.GatewayProperties;
 
